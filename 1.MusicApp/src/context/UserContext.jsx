748d0c2abd4bfc5faeaf2725ae6640b4fa156be9
Prince <princegupta1455@gmail.com>
2025-03-15 05:39:16 +0530

    created prev next on off button

--- 1.MusicApp/src/context/UserContext.jsx
+++ 1.MusicApp/src/context/UserContext.jsx
@@ -1,10 +1,54 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useRef, useState } from 'react'
+import { songsData } from '../songs'
 
 export const dataContext=createContext()
 
 function UserContext({children}) {
+  let audioRef=useRef(new Audio())
+  let [index , setIndex]=useState(0)
+  let [playingSong,setPlayingSong]=useState(false);
+
+  
+
+    useEffect(()=>{
+      audioRef.current.src=songsData[index].song
+      audioRef.current.load()
+      if(playingSong){
+        playSong()
+      }
+    },[index])
+
+    function playSong(){
+      setPlayingSong(true)
+      audioRef.current.play()
+    }
+    function pauseSong(){
+      setPlayingSong(false)
+      audioRef.current.pause()
+    }
+    function nextSong(){
+      setIndex(prev=>(prev+1)%songsData.length)
+    }
+    function prevSong(){
+      setIndex(prev=>{
+        if(prev===0){
+          return songsData.length-1
+        }else{
+          return prev-1;
+        }
+      })
+    }
+
     let value={
-        
+        audioRef,
+        playSong,
+        pauseSong,
+        playingSong,
+        setPlayingSong,
+        nextSong,
+        setIndex,
+        index,
+        prevSong
     }
   return (
    
--- 1.MusicApp/src/pages/Home.jsx
+++ 1.MusicApp/src/pages/Home.jsx
@@ -1,46 +1,74 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { songsData } from '../songs'
 import musicgif from '../assets/musicanim.webp';
 import { CgPlayTrackPrev } from "react-icons/cg";
 import { CgPlayTrackNext } from "react-icons/cg";
 import { FaPlay } from "react-icons/fa";
 import { FaPause } from "react-icons/fa";
+import { dataContext } from '../context/userContext';
+
 
 
 function Home() {
+
+    let {
+        audioRef,
+        playSong,
+        pauseSong,
+        playingSong,
+        setPlayingSong,
+        nextSong,
+        setIndex,
+        index,
+        prevSong
+    } =useContext(dataContext)
+    
+
     return (
         <div className='w-full h-screen bg-black flex'>
 
             <div className='md:w-[50%] w-full h-full  flex justify-start items-center pt-[20px] md:pt-[120px] flex-col gap-[30px]'>
 
                 <h1 className='text-white font-semibold text-[20px]'>Now Playing</h1>
-                <div className='w-[80%] md:w-[250px] h-[300px] object-fill rounded-md overflow-hidden relative '>
-                    <img src={songsData[0].image} alt='' />
+
+                <div className='w-[80%] md:w-[250px] h-[250px] object-fill rounded-md overflow-hidden relative '>
+                    <img src={songsData[index].image} alt='' className='w-[100%] h-[100%]'/>
                     <div className='w-full h-full bg-black absolute top-0 opacity-[0.5] flex justify-center items-center'>
                         <img src={musicgif} alt='' className='w-[55%] h-[30%]' />
                     </div>
 
                 </div>
+
                 <div>
-                    <div className='text-white text-[30px] font-bold text-center'>{songsData[0].name} </div>
-                    <div className='text-gray-400 text-[18px] text-center'>{songsData[0].singer} </div>
+                    <div className='text-white text-[30px] font-bold text-center'>{songsData[index].name} </div>
+                    <div className='text-gray-400 text-[18px] text-center'>{songsData[index].singer} </div>
                 </div>
+
                 <div className='w-full flex justify-center items-center'>
                     <input type="range" className='appearance-none w-[50%] h-[7px] rounded-md bg-gray-600' id='range' />
                 </div>
+
                 <div className='text-white flex justify-center items-center gap-5'>
-                    <CgPlayTrackPrev className='w-[28px] h-[28px]  hover:text-gray-600 transition-all cursor-pointer' />
-                    <div className='w-[50px] h-[50px] rounded-full bg-white text-black flex justify-center items-center hover:bg-gray-600 transition-all cursor-pointer'>
-                    
-                    <FaPlay className='w-[20] '/>
-                       
+
+                    <CgPlayTrackPrev className='w-[28px] h-[28px]  hover:text-gray-600 transition-all cursor-pointer' onClick={()=>prevSong()} />
+
+                    {!playingSong?
+                    <div className='w-[50px] h-[50px] rounded-full bg-white text-black flex justify-center items-center hover:bg-gray-600 transition-all cursor-pointer ' onClick={()=>playSong()}>
+                    <FaPlay className='w-[20px] '/>
                     </div>
-                    <CgPlayTrackNext className='w-[28px] h-[28px]  hover:text-gray-600 transition-all cursor-pointer' />
+                        :
+                    <div className='w-[50px] h-[50px] rounded-full bg-white text-black flex justify-center items-center hover:bg-gray-600 transition-all cursor-pointer' onClick={()=>pauseSong()}>
+                    <FaPause className='w-[20px]' />
+                    </div>
+                    }
+
+                    <CgPlayTrackNext className='w-[28px] h-[28px]  hover:text-gray-600 transition-all cursor-pointer' onClick={()=>nextSong()} />
 
                 </div>
 
 
             </div>
+
             <div className='w-[50%] h-full bg-slate-700 hidden md:flex'>
 
 
